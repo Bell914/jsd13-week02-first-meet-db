9e8b1b2fd0b619f212b56e912bab0dafbcad0acc
Bell914 <ittikorn.tipson@gmail.com>
2026-07-06 16:20:20 +0700

    add update03

--- query2.mongodb.js
+++ query2.mongodb.js
@@ -1,5 +1,30 @@
 use("sample_mflix");
-db.movies.find({ plot: { $regex: "street.$", $options: "i" } }).count()
+
+// use("sample_mflix");
+
+// db.movies.find({ type: "movie", rated: "TV-G" }).count();
+
+// use("sample_mflix");
+
+//  db.movies.find({ type: "movie", rated: "TV-G" });
+
+// use("sample_mflix");
+
+// db.theaters.find({ "location.address.state": "AL" }).count()
+
+// use("sample_mflix");
+
+// db.theaters.find({ "location.address.city": "La Quinta" }).count()
+
+// use("sample_mflix");
+
+// db.theaters.findOne({ "location.address.city": "La Quinta" })
+
+// use("sample_mflix");
+
+// db.movies.find({ plot: { $regex: "American", $options: "i" } }).count()q
+
+
 
 
 
